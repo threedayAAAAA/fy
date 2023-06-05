532dbcfbc0de78e38587117cc95f65ab7e0e5518
柳 <64195205+liu839@users.noreply.github.com>
2023-06-05 03:03:10 +0000

    使用spyOn劫持random测试

--- __test__/boolean.spec.ts
+++ __test__/boolean.spec.ts
@@ -1,10 +1,24 @@
 import { randomBool } from '@/base/boolean'
 
 describe('randomBool', () => {
-    it('应该以50%的概率生成true或false', () => {
-        const result = randomBool()
-        expect(result).toBeTypeOf('boolean')
+    afterEach(() => {
+      vi.restoreAllMocks()
     })
+    
+    it('应该以50%的概率生成true或false', () => {
+      vi.spyOn(Math, 'random').mockReturnValue(0.4)
+      expect(randomBool()).toBe(false)
+      vi.spyOn(Math, 'random').mockReturnValue(0.6)
+      expect(randomBool()).toBe(true)
+    })
+
+    it('当指定当前值时,应该以max/(max+min)的概率生成当前值，以min/(max+min)的概率生成相反的值', () => {
+      vi.spyOn(Math, 'random').mockReturnValue(0.60)
+      expect(randomBool(true, 1, 9)).toBe(true)
+      vi.spyOn(Math, 'random').mockReturnValue(0.91)
+      expect(randomBool(true, 1, 9)).toBe(false)
+    })
+  
 
     it('当min为0时，应该总是生成当前值', () => {
         const current = false
